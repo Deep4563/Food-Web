3005757e8f38421dcfaba92e6a1a6b6970c7bb6d
chiragbhisikar <developerxchirag@gmail.com>
2025-03-08 17:49:35 +0530

    Project Completed
    Project Completed

---
+++ Frontend/src/Context/StoreContext.jsx
@@ -0,0 +1,104 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+// import { food_list } from "../assets/assets 2/frontend_assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props)=>{
+
+    const [cartItems,setCartItems] = useState({});
+    const url = "http://localhost:4563"
+    const [token,setToken] = useState("");
+    const [food_list,setFoodList] = useState([]);
+
+    const addToCart = async (itemId)=>{
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+        //add cart in database
+        if (token) {
+              await axios.post(
+              url + "/api/cart/add",
+              { itemId },
+              { headers: { token } }
+            );
+          }
+
+    }
+
+    const removeFromCart = async (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+
+        //removecart in database
+        if (token) {
+            await axios.post(
+              url + "/api/cart/remove",
+              { itemId },
+              { headers: { token } }
+            );
+          }
+    }
+
+    // useEffect(()=>{
+    //     console.log(cartItems);
+    // },[cartItems])
+
+    const getTotalCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item] > 0){
+
+                let itemInfo = food_list.find((product)=>product._id===item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+
+    const fetchFoodList = async()=>{
+        const response = await axios.get(url+"/api/food/list");
+        console.log(response.data);
+        setFoodList(response.data.data);
+
+    }
+
+    //fix we reload web page cart information stay
+    const loadCartData = async (token)=>{
+
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+    }
+
+    useEffect(() => {
+        async function loadData(){
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+              setToken(localStorage.getItem("token")); //reload web page not logout
+              await loadCartData(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+      }, []);
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
+    }
+    return (
+        <StoreContext.Provider value={contextValue} >
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+export default StoreContextProvider; 
